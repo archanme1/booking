19ecbe72af352573480841fb825f56042e801576
archan bhatta <archanme1@gmail.com>
2022-05-30 18:30:27 -0400

    mongo authentication for login and register

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRoute from "./routes/auth.js";
 import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
@@ -19,7 +20,7 @@ const connect = async () => {
   }
 };
 
-// mongoose listing
+// mongoose listening
 mongoose.connection.on("disconnected", () => {
   console.log("Mongodb is disconnected");
 });
@@ -28,13 +29,29 @@ mongoose.connection.on("connected", () => {
 });
 
 // middlewares
+// to use json
+app.use(express.json());
+// to use cookie
+app.use(cookieParser());
+// endpoints
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
+// error middleware
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "SOmething went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
 
 // backend connection
 app.listen(8800, () => {
   connect();
-  console.log("Backend is connected!");
+  console.log("Backend is connected! at 8800");
 });
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,23 @@
 import express from "express";
+import {
+  deleteUser,
+  getAllUser,
+  getUser,
+  updateUser,
+} from "../controllers/users";
 
 const router = express.Router();
+
+// UPDATE
+router.put("/:id", updateUser);
+
+// DELETE
+router.delete("/:id", deleteUser);
+
+// GET
+router.get("/:id", getUser);
+
+// GET ALL
+router.get("/", getAllUser);
 
 export default router;
